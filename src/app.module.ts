3973922fed378cecec9fa878065064eb36be59fc
Dorina Gecseg <gecseg.dorina@gmail.com>
2022-07-25 17:52:21 +0200

    add typeorm, create server environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Config } from './config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRoot(Config.getOrmConfig()),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import "reflect-metadata"
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  migrationsTableName: 'migrations',
+  type: process.env.TYPEORM_CONNECTION as any,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT as any,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+  entities: ['.dist/src/database/entities/*.entity.js'],
+  migrations: ['./dist/src/database/migrations/*.js'],
+});
