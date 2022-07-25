818012476bd3b7184533815d78dd2db2f02f0400
Dorina Gecseg <gecseg.dorina@gmail.com>
2022-07-25 15:59:45 +0200

    add swagger, add configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+import packageJson from '../package.json';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+        .setTitle(packageJson.title)
+        .setDescription(packageJson.description)
+        .setVersion(packageJson.version)
+        .build();
+
+    const configService = app.get(ConfigService);
+
+    const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup(configService.get('DOCUMENTATION_URL') || 'swagger', app, document);
+
+    await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
+
